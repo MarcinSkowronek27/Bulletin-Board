2c9c47a2102b5203c463fa34c9ee2a2d586a726a
Marcin Skowronek <marcinfoto35@op.pl>
2021-11-16 16:53:07 +0100

    NotFound Done/add datas in initstate

--- src/redux/initialState.js
+++ src/redux/initialState.js
@@ -1,6 +1,32 @@
 export const initialState = {
   posts: {
-    data: {},
+    data: [
+      {
+        id: 1,
+        title: 'New skateboard!',
+        text: 'Lorem ipsum dolom mites',
+        created: '2021-11-16',
+        updated: '2021-11-16',
+        email: 'johnDoe@example.com',
+        status: 'published',
+        image: 'https://images.pexels.com/photos/165236/pexels-photo-165236.jpeg?cs=srgb&dl=pexels-khoa-hu%E1%BB%B3nh-165236.jpg&fm=jpg',
+        price: '250PLN',
+        phone: '111333222',
+        location: 'Poland',
+      },
+      {
+        id: 2,
+        title: 'Get this lamp!',
+        text: 'Lorem ipsum dolom mites',
+        created: '2021-11-14',
+        updated: '2021-11-15',
+        email: 'johnDoe@example.com',
+        status: 'draft',
+        image: 'https://images.pexels.com/photos/1112598/pexels-photo-1112598.jpeg?cs=srgb&dl=pexels-eneida-nieves-1112598.jpg&fm=jpg',
+        price: '120PLN',
+        phone: '111333555',
+        location: 'Poland',
+      }],
     loading: {
       active: false,
       error: false,
