5bb462b8f09ee6da457092cbb601506f089e9f2c
Marcin Skowronek <marcinfoto35@op.pl>
2021-11-29 21:12:01 +0100

    AddPost in postsRedux

--- src/redux/postsRedux.js
+++ src/redux/postsRedux.js
@@ -13,12 +13,14 @@ const FETCH_START = createActionName('FETCH_START');
 const FETCH_SUCCESS = createActionName('FETCH_SUCCESS');
 const FETCH_ERROR = createActionName('FETCH_ERROR');
 const GET_ONE_POST = createActionName('GET_ONE_POST');
+const ADD_POST = createActionName('ADD_PHOTO');
 
 /* action creators */
 export const fetchStarted = payload => ({ payload, type: FETCH_START });
 export const fetchSuccess = payload => ({ payload, type: FETCH_SUCCESS });
 export const fetchError = payload => ({ payload, type: FETCH_ERROR });
 export const fetchOnePost = payload => ({ payload, type: GET_ONE_POST });
+export const addPost = payload => ({ payload, type: ADD_POST });
 
 /* thunk creators */
 export const fetchPublished = () => {
@@ -48,6 +50,22 @@ export const fetchPostById = (id) => {
       .get(`http://localhost:8000/api/posts/${id}`)
       .then(res => {
         dispatch(fetchOnePost(res.data));
+      })
+      .catch(err => {
+        dispatch(fetchError(err.message || true));
+      });
+  };
+};
+
+export const addPostRequest = (post) => {
+
+  const newPost = { ...post };
+  return (dispatch, getState) => {
+    dispatch(fetchStarted());
+    Axios
+      .get('http://localhost:8000/api/posts/add', newPost)
+      .then(res => {
+        dispatch(addPost(res.data));
       })
       .catch(err => {
         dispatch(fetchError(err.message || true));
@@ -94,6 +112,12 @@ export const reducer = (statePart = [], action = {}) => {
           error: false,
         },
         onePost: action.payload,
+      };
+    }
+    case ADD_POST: {
+      return {
+        ...statePart,
+        data: [...statePart.data, action.payload],
       };
     }
     default:
