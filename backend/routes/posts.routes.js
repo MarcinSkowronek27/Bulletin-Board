12f9e0aef6fce8c12c728e90ce635d973eeadba7
Marcin Skowronek <marcinfoto35@op.pl>
2021-11-30 09:59:59 +0100

    Add validation on server side

--- backend/routes/posts.routes.js
+++ backend/routes/posts.routes.js
@@ -6,13 +6,13 @@ const Post = require('../models/post.model');
 router.get('/posts', async (req, res) => {
   try {
     const result = await Post
-      .find({status: 'published'})
+      .find({ status: 'published' })
       .select('author created title photo')
-      .sort({created: -1});
-    if(!result) res.status(404).json({ post: 'Not found' });
+      .sort({ created: -1 });
+    if (!result) res.status(404).json({ post: 'Not found' });
     else res.json(result);
   }
-  catch(err) {
+  catch (err) {
     res.status(500).json(err);
   }
 });
@@ -21,10 +21,33 @@ router.get('/posts/:id', async (req, res) => {
   try {
     const result = await Post
       .findById(req.params.id);
-    if(!result) res.status(404).json({ post: 'Not found' });
+    if (!result) res.status(404).json({ post: 'Not found' });
     else res.json(result);
   }
-  catch(err) {
+  catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.post('/posts/add', async (req, res) => {
+  try {
+    const { author, title, text, photo, created, updated, status, price, phone, location } = req.body;
+
+    if (created && updated && status && title.length >= 10 && text.length >= 20 && author.includes('@')) {
+      // res.json({ message: 'OK' });
+      const newPost = new Post({
+        author, created, title, updated, text, photo, phone, location, status, price,
+      });
+      await newPost.save();
+      if (!newPost) res.status(404).json({ post: 'Not found' });
+      else res.json(newPost);
+    }
+    else {
+      // res.json({ message: 'something went wrong' });
+      throw new Error('Wrong input!');
+    }
+  }
+  catch (err) {
     res.status(500).json(err);
   }
 });
